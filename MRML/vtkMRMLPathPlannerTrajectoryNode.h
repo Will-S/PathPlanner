682bc3de6b35f2b5fe480578ff2813c6262a3708
Laurent Chauvin <lchauvin@bwh.harvard.edu>
2013-05-20 13:25:40 -0400

    STYLE: Updated headers

--- MRML/vtkMRMLPathPlannerTrajectoryNode.h
+++ MRML/vtkMRMLPathPlannerTrajectoryNode.h
@@ -1,3 +1,25 @@
+/*==============================================================================
+
+  Program: 3D Slicer
+ 
+  Portions (c) Copyright Brigham and Women's Hospital (BWH) All Rights Reserved.
+ 
+  See COPYRIGHT.txt
+  or http://www.slicer.org/copyright/copyright.txt for details.
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ 
+  This file was originally developed by Laurent Chauvin, Brigham and Women's
+  Hospital. The project was supported by grants 5P01CA067165,
+  5R01CA124377, 5R01CA138586, 2R44DE019322, 7R01CA124377,
+  5R42CA137886, 8P41EB015898
+ 
+==============================================================================*/
+
 #ifndef __vtkMRMLPathPlannerTrajectoryNode_h
 #define __vtkMRMLPathPlannerTrajectoryNode_h
 
